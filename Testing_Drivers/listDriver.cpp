739b3ab5bed2103e576921c6dfafcce5d9f019ca
Alex Edwards <edwardsar65@gmail.com>
2015-05-28 22:12:37 -0700

    more test cases

--- Testing_Drivers/listDriver.cpp
+++ Testing_Drivers/listDriver.cpp
@@ -3,11 +3,25 @@
 int main(void){
   List<int> intList;
   List<int>::Iterator itr;
-  intList.append(10);
-  intList.prepend(3);
+  intList.append(2);
+  intList.append(3);
+  intList.prepend(1);
+  intList.prepend(0);
+  intList.prepend(8);
   itr = intList.getIterator();
   for(itr.startAtHead();!itr.end();itr.getNext()){
-    std::cout << itr.getData() << std::endl;
+    std::cout << itr.getData() << ", ";
   }
+  std::cout << std::endl;
+
+  intList.remove(8);
+  intList.remove(2);
+
+  for(itr.startAtTail();!itr.end();itr.getPrev()){
+    std::cout << itr.getData() << ", ";
+  }
+  std::cout << std::endl;
+  std::cout << intList.search(3) << std::endl;
+  std::cout << intList.search(8) << std::endl;
   return 0;
 }
--- Testing_Drivers/stackDriver.cpp
+++ Testing_Drivers/stackDriver.cpp
@@ -3,8 +3,16 @@
  */
 
 #include "../Stack.hpp"
+#include <iostream>
 
 int main(void){
   Stack<int> intStack;
+  intStack.push(3);
+  intStack.push(2);
+  intStack.push(1);
+
+  while(!intStack.isEmpty()){
+    std::cout << intStack.pop() << std::endl;
+  }
   return 0;
 }
