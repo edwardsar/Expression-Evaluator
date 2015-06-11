9fdca455cb65e41117dd6d8b32f1aa71d46d1465
Alex Edwards <edwardsar65@gmail.com>
2015-06-11 08:46:02 -0700

    test cases for stack

--- Testing_Drivers/stackDriver.cpp
+++ Testing_Drivers/stackDriver.cpp
@@ -7,12 +7,21 @@
 
 int main(void){
   Stack<int> intStack;
-  intStack.push(3);
+
   intStack.push(2);
-  intStack.push(1);
+  intStack.pop();
+
+  int i = 0;
+  while(i<100){
+    intStack.push(i);
+    i++;
+  }
+
+
 
   while(!intStack.isEmpty()){
-    std::cout << intStack.pop() << std::endl;
+    std::cout << intStack.pop() << ", ";
   }
+  std::cout << std::endl;
   return 0;
 }
